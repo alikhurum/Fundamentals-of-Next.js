ff92e0a509fbf6f97ed542c0e1ff4c331fadcd8b
Tayyeb Ahmad <alikhurum50@gmail.com>
2026-07-21 04:17:50 -0700

    Tailwind Setup & Alert Component

---
+++ Styling in Next.js/Task 6. Tailwind Setup & Alert Component/Alert.js
@@ -0,0 +1,7 @@
+export default function Alert() {
+  return (
+    <div className="bg-red-100 text-red-700 p-4 rounded-lg border border-red-300 my-4">
+      ⚠️ This is an alert message!
+    </div>
+  );
+}
